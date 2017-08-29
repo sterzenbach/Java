d2241f4194795dc6d4a1c657860f0fb708075b8c
sterzenb <sterzenb@DESKTOP-V8AU0QO>
2017-08-29 21:02:19 +0200

    V2

--- Test88/src/ab02/Hallo.java
+++ Test88/src/ab02/Hallo.java
@@ -5,6 +5,7 @@ public class Hallo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Dies ist eine andere Version");
+		System.out.println("Siehe da...");
 
 	}
 
