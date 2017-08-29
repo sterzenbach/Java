cf94fabafcf2cc911c6708f225f0aa619cb63638
sterzenb <sterzenb@DESKTOP-V8AU0QO>
2017-08-29 21:01:24 +0200

    Grund

---
+++ Test88/src/ab02/Hallo.java
@@ -0,0 +1,11 @@
+package ab02;
+
+public class Hallo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Dies ist eine andere Version");
+
+	}
+
+}
